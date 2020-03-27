5252811a78e9b01a6ecaa08204ca9499a5a745d5
NoHaxJustSoFlo <58442140+NoHaxJustSoFlo@users.noreply.github.com>
2020-03-27 13:20:30 +0100

    Meksicka: Napisana klasa Prilog

--- Meksicka/main.cpp
+++ Meksicka/main.cpp
@@ -58,15 +58,60 @@ void IspisiMeso(const Meso& meso)
         vrstaMesa = "Piletina";
         break;
     case BEZ_MESA:
-        vrstaMesa = "Bez mesa";
+        cout << "Bez mesa" << endl;
+        return;
     }
     cout << vrstaMesa << " " << meso.GetGramaza() << endl;
 }
 
+enum VrstaPriloga{ RIZA, POMFRIT, PASULJ, TESTENINA, BEZ_PRILOGA };
+
 class Prilog
 {
+private:
+    VrstaPriloga vrstaPriloga;
 
+public:
+    Prilog()
+    {
+        vrstaPriloga = BEZ_PRILOGA;
+    }
+    Prilog(const Prilog& prilog)
+    {
+        vrstaPriloga = prilog.vrstaPriloga;
+    }
+    void SetPrilog(VrstaPriloga _vrstaPriloga)
+    {
+        vrstaPriloga = _vrstaPriloga;
+    }
+    VrstaPriloga GetPrilog()const
+    {
+        return vrstaPriloga;
+    }
 };
+
+void IspisiPrilog(const Prilog& prilog)
+{
+    switch(prilog.GetPrilog())
+    {
+    case POMFRIT:
+        cout << "Pomfrit";
+        break;
+    case RIZA:
+        cout << "Riza";
+        break;
+    case PASULJ:
+        cout << "Pasulj";
+        break;
+    case TESTENINA:
+        cout << "Testenina";
+        break;
+    case BEZ_PRILOGA:
+        cout << "Bez priloga";
+        break;
+    }
+    cout << endl;
+}
 
 class Obrok
 {
